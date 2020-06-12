be39ab7899161da8dd787bc06c9703d29a7e59c9
junk <66475075+a7p11t@users.noreply.github.com>
2020-06-12 15:44:21 +0900

    [FEAT] Add Glibc build script to run.sh

--- run.sh
+++ run.sh
@@ -86,3 +86,28 @@ tar -xf linux-*.tar.xz -C /tmp/ \
   && popd \
   && rm -rf /tmp/linux
 #}}}
+
+#{{{ Build Glibc
+tar -xf glibc-*.tar.xz -C /tmp/ \
+  && mv /tmp/glibc-* /tmp/glibc \
+  && pushd /tmp/glibc \
+  && mkdir -v build \
+  && cd build \
+  && ../configure                       \
+    --prefix=/tools                    \
+    --host=$LFS_TGT                    \
+    --build=$(../scripts/config.guess) \
+    --enable-kernel=3.2                \
+    --with-headers=/tools/include      \
+  && make \
+  && make install \
+  && popd \
+  && rm -rf /tmp/glibc
+
+# perform a sanity check that basic functions (compiling and linking)
+# are working as expected
+echo 'int main(){}' > dummy.c \
+  && $LFS_TGT-gcc dummy.c \
+  && readelf -l a.out | grep ': /tools' \
+  && rm -v dummy.c a.out
+#}}}

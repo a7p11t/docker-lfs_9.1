6a2508fc3e422524ec4bacf649eb85ab0a23956f
junk <66475075+a7p11t@users.noreply.github.com>
2020-06-12 15:37:56 +0900

    [FEAT] Add Binutils first build script to run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 set -eu
+
+#{{{ Build binutils(1)
+echo "Building binutils..."
+
+tar -xf binutils-*.tar.xz -C /tmp/ && \
+    mv /tmp/binutils-* /tmp/binutils && \
+    pushd /tmp/binutils && \
+    mkdir -v build && \
+    cd build && \
+    ../configure               \
+    --prefix=/tools            \
+    --with-sysroot=$LFS        \
+    --with-lib-path=/tools/lib \
+    --target=$LFS_TGT          \
+    --disable-nls              \
+    --disable-werror        && \
+    make && \
+    mkdir -pv /tools/lib && \
+    ln -sv lib /tools/lib64 && \
+    make install && \
+    popd && \
+    rm -rf /tmp/binutils
+#}}}
